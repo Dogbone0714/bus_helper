a38347f2d7395588c93dee8c342b12800c97a461
Dogbone0714 <jameskang0714@gmail.com>
2025-03-02 17:38:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,5 @@ def get_next_bus():
     return jsonify({"result": result})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
